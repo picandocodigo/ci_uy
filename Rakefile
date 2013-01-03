5e34e3561008c4f4e1421d3cef18489ae835629f
Fernando Briano <fernando@picandocodigo.net>
2013-01-03 14:29:46 -0200

    Adds license and tests

---
+++ Rakefile
@@ -0,0 +1,5 @@
+require 'rake/testtask'
+
+Rake::TestTask.new("test") do |t|
+  t.pattern = "test/**/*_test.rb"
+end
---
+++ test/ci_uy_test.rb
@@ -0,0 +1,32 @@
+require 'minitest/spec'
+require 'minitest/pride'
+require 'minitest/autorun'
+require 'ci_uy'
+
+describe CiUY do
+  it "should return a valid checker digit" do
+    ci = "1111111"
+    assert 1, CiUY.get_validation_digit(ci)
+
+    ci = "2222222"
+    assert 2, CiUY.get_validation_digit(ci)
+  end
+
+  it "should validate numbers" do
+    ci = "11111111"
+    assert CiUY.validate_ci(ci)
+  end
+
+  it "should validate numbers even when using dots and dashes" do
+    ci = "1.111.111-1"
+    assert CiUY.validate_ci(ci)
+
+    ci = "1-111/111/1"
+    assert CiUY.validate_ci(ci)
+  end
+
+  it "should not validate wrong numbers" do
+    ci = "1.111.222.1"
+    assert !CiUY.validate_ci(ci)
+  end
+end
